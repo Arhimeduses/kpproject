377da678410dde2726cdf73e60af670f8471dbad
Artem <m10a3o99@gmail.com>
2023-12-03 22:33:05 +0300

    Настроил валидацию

--- front/src/App.vue
+++ front/src/App.vue
@@ -4,16 +4,34 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <body>
-    
     <div class="rightbuttons">
-      <button class="rightbuttons-elements" type="submit"><RouterLink to="/">Персональные данные</RouterLink></button>
-      <button class="rightbuttons-elements" type="submit"><RouterLink to="/OrdersHistory">История заказов</RouterLink></button>
-      <button class="rightbuttons-elements" type="submit"><RouterLink to="/PasswordChange">Смена пароля</RouterLink></button>
+      <RouterLink class="rightbuttons-elements" to="/">Персональные данные</RouterLink>
+      <RouterLink class="rightbuttons-elements" to="/OrdersHistory">История заказов</RouterLink>
+      <RouterLink class="rightbuttons-elements" to="/ChangePassword">Смена пароля</RouterLink>
     </div>
     <div style="margin-right: 300px;">
       <RouterView />
     </div>
   </body>
-  
 </template>
 
+<style scoped>
+.rightbuttons {
+  display: flex;
+  flex-direction: column;
+  gap: 1cm; /* Расстояние между кнопками */
+}
+
+.rightbuttons-elements {
+  text-decoration: none;
+  color: white; /* Белый цвет текста */
+  background-color: grey; /* Черный цвет фона */
+  padding: 10px; /* Поля вокруг текста для создания кнопки */
+  text-align: center; /* Выравнивание текста по центру */
+  cursor: pointer;
+}
+
+.rightbuttons-elements:hover {
+  text-decoration: underline;
+}
+</style>
