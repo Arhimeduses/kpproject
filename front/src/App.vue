b78d9563b82f91cfd6026fc8cfd1eb9a64553848
Софья Улитина <wdusgs76r@gmail.com>
2023-12-14 21:21:06 +0300

    commit

--- front/src/App.vue
+++ front/src/App.vue
@@ -8,7 +8,6 @@ import { RouterLink, RouterView } from 'vue-router'
       <RouterLink class="rightbuttons-elements" to="/">Персональные данные</RouterLink>
       <RouterLink class="rightbuttons-elements" to="/OrdersHistory">История заказов</RouterLink>
       <RouterLink class="rightbuttons-elements" to="/PasswordChange">Смена пароля</RouterLink>
-      <RouterLink class="rightbuttons-elements" to="/LoginPage">Выход</RouterLink>
     </div>
     <div style="margin-right: 300px;">
       <RouterView />
